0145080277ac712e59a843e3f802c8b84d36d3e7
Veraprot <vera.protopopova@gmail.com>
2018-10-09 17:29:21 -0400

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
-require 'pry'
-
 def my_find(collection)
 
+end
+
+def my_find(collection)
+  current_index = 0 
+  while current_index < collection.count
+    return collection[current_index] if yield(collection[current_index])
+    current_index += 1
+  end 
 end
