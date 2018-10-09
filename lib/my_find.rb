a22a0a87f27cff383ec03b294b77e8d5bada105e
yoshinator <yoanante@gmail.com>
2018-10-09 15:03:17 -0400

    my find

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_find(collection)
-
+  hash = Hash.new(0)
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    end
+    i+=1
+  end
 end
+
+binding.pry
